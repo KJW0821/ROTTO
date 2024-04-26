3092d2f8f4f386bb92bd2e3e8b9678cfb25e94dc
KJWKJW <force1394@gmail.com>
2024-04-26 16:27:57 +0900

    chore: back spring boot 프로젝트 생성

---
+++ back/src/main/java/com/rezero/rotto/RottoApplication.java
@@ -0,0 +1,13 @@
+package com.rezero.rotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottoApplication.class, args);
+	}
+
+}
