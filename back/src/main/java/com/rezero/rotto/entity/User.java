4b9dab81b89d3e96d5f70e744e1c8cc472eb548a
KJWKJW <force1394@gmail.com>
2024-04-27 20:34:22 +0900

    feat: 유저 엔티티 생성

---
+++ back/src/main/java/com/rezero/rotto/entity/User.java
@@ -0,0 +1,50 @@
+package com.rezero.rotto.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@Table(name = "user_tb")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_code")
+    int userCode;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "sex")
+    String sex;
+
+    @Column(name = "pin")
+    String pin;
+
+    @Column(name = "phone_num")
+    String phoneNum;
+
+    @Column(name = "jumin_no")
+    String juminNo;
+
+    @Builder.Default
+    @Column(name = "is_delete")
+    Boolean isDelete = false;
+
+    @CreationTimestamp
+    @Column(name = "join_time")
+    Timestamp joinTime;
+
+    @Column(name = "delete_time")
+    Timestamp deleteTime;
+
+
+}
