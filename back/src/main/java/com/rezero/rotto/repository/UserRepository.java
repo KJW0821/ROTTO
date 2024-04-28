e5cca3054bfeee65fbc123d4367f52c58d10622b
강주원 <wizuded@naver.com>
2024-04-28 22:51:08 +0900

    feat: 유저 리포지토리 작성

---
+++ back/src/main/java/com/rezero/rotto/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.rezero.rotto.repository;
+
+import com.rezero.rotto.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByPhoneNumAndPin(String phoneNum, String pin);
+
+    Optional<User> findByPhoneNum(String phoneNum);
+
+    User findByUserCode(int userCode);
+
+}
