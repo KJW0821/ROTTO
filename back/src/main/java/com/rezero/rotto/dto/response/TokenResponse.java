c6ac32060200829d528462356dbdf6a20fe0927e
KJWKJW <force1394@gmail.com>
2024-04-28 22:48:07 +0900

    refactor: TokenDto -> TokenResponse 로 rename & dto/response 로 경로 변경

--- back/src/main/java/com/rezero/rotto/dto/response/TokenResponse.java
+++ back/src/main/java/com/rezero/rotto/dto/response/TokenResponse.java
@@ -1,11 +1,11 @@
-package com.rezero.rotto.dto.dto;
+package com.rezero.rotto.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
-public class TokenDto {
+public class TokenResponse {
 
     private String grantType; // Bearer
     private String accessToken;
