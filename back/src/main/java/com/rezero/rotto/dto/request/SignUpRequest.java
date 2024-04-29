d0d048ec44a0ebb3e3b00cfd8ae7a227985ac2e7
강주원 <wizuded@naver.com>
2024-04-29 17:16:15 +0900

    refactor: 회원가입시 필수 필드, 필드 조건 추가

--- back/src/main/java/com/rezero/rotto/dto/request/SignUpRequest.java
+++ back/src/main/java/com/rezero/rotto/dto/request/SignUpRequest.java
@@ -1,14 +1,24 @@
 package com.rezero.rotto.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class SignUpRequest {
 
+    @NotBlank(message = "이름은 필수입니다.")
+    @Size(max = 20, message = "이름은 20자 이하여야 합니다.")
     String name;
+    @NotBlank(message = "성별은 필수입니다.")
+    @Pattern(regexp = "^[MF]$", message = "성별은 M 또는 F 이어야 합니다.")
     String sex;
-    String pin;
+    @NotBlank(message = "휴대폰 번호는 필수입니다.")
+    @Size(min = 11, max = 11, message = "휴대폰 번호는 11자리 숫자여야 합니다.")
     String phoneNum;
+    @NotBlank(message = "주민등록번호는 필수입니다.")
+    @Pattern(regexp = "[0-9]{6}", message = "주민등록번호는 6자리 숫자여야 합니다.")
     String juminNo;
 
 }
