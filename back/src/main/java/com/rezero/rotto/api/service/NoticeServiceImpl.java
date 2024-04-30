7a8809953181d6f01f45a4c24c229e7e47f4ce09
KJWKJW <force1394@gmail.com>
2024-04-30 14:37:35 +0900

    refactor: Notice 목록 조회 기능 구현

---
+++ back/src/main/java/com/rezero/rotto/api/service/NoticeServiceImpl.java
@@ -0,0 +1,78 @@
+package com.rezero.rotto.api.service;
+
+import com.rezero.rotto.dto.dto.NoticeListDto;
+import com.rezero.rotto.dto.response.NoticeListResponse;
+import com.rezero.rotto.entity.Notice;
+import com.rezero.rotto.entity.User;
+import com.rezero.rotto.repository.NoticeRepository;
+import com.rezero.rotto.repository.UserRepository;
+import com.rezero.rotto.utils.Pagination;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class NoticeServiceImpl implements NoticeService {
+
+    private final UserRepository userRepository;
+    private final NoticeRepository noticeRepository;
+    private final Pagination pagination;
+
+
+    // 공지사항 목록 조회
+    public ResponseEntity<?> getNoticeList(int userCode, Integer page) {
+        // 해당 유저가 존재하는지 검사
+        User user = userRepository.findByUserCode(userCode);
+        if (user == null || user.getIsDelete()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
+        }
+
+        // 공지사항 모두 불러오기
+        List<Notice> notices = noticeRepository.findAll();
+        List<NoticeListDto> noticeListDtos = new ArrayList<>();
+
+        // 인덱스 선언
+        int startIdx = 0;
+        int endIdx = 0;
+        // 총 페이지 수 선언
+        int totalPages = 1;
+
+        // 페이지네이션
+        List<Integer> indexes = pagination.pagination(page, 10, notices.size());
+        startIdx = indexes.get(0);
+        endIdx = indexes.get(1);
+        totalPages = indexes.get(2);
+
+        // 최신것부터 보여주기 위해 리스트 뒤집기
+        Collections.reverse(notices);
+        // Notice 리스트 페이지네이션
+        List<Notice> pageNotices = notices.subList(startIdx, endIdx);
+        // Notice 리스트를 순회
+        for (Notice notice : pageNotices) {
+            // Dto 에 담기
+            NoticeListDto noticeListDto = NoticeListDto.builder()
+                    .noticeCode(notice.getNoticeCode())
+                    .title(notice.getTitle())
+                    .createTime(notice.getCreateTime())
+                    .build();
+            // noticeListDtos 에 담기
+            noticeListDtos.add(noticeListDto);
+        }
+
+        // 리스폰스 생성
+        NoticeListResponse response = NoticeListResponse.builder()
+                .noticeListDtos(noticeListDtos)
+                .totalPages(totalPages)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+}
