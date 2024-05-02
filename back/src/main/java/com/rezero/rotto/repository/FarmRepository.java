b28778cc09d3a1cd6c395c4f49b24142829556a8
강주원 <wizuded@naver.com>
2024-05-02 10:25:46 +0900

    feat: Farm 리포지토리 작성

---
+++ back/src/main/java/com/rezero/rotto/repository/FarmRepository.java
@@ -0,0 +1,7 @@
+package com.rezero.rotto.repository;
+
+import com.rezero.rotto.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmRepository extends JpaRepository<Farm, Integer> {
+}
