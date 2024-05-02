2c0eee6ae3f94c96cd0a7e9f4861397ea33ebfa6
KJWKJW <force1394@gmail.com>
2024-05-02 11:32:17 +0900

    feat: Bean 리포지토리 생성

---
+++ back/src/main/java/com/rezero/rotto/repository/BeanRepository.java
@@ -0,0 +1,9 @@
+package com.rezero.rotto.repository;
+
+import com.rezero.rotto.entity.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeanRepository extends JpaRepository<Bean, Integer> {
+}
