c3ebe3856e3ec850f69e0728bfed8cf388f946bd
손유정 <sonyujeong18@gmail.com>
2024-05-02 14:33:17 +0900

    feat: 네비게이션 설정

--- front/App.js
+++ front/App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { store } from './src/stores/store';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthRouters from './src/routers/AuthRouters';
+import Constants from 'expo-constants';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'pretendard-thin': require('./assets/fonts/Pretendard-Thin.ttf'),
+    'pretendard-extraLight': require('./assets/fonts/Pretendard-ExtraLight.ttf'),
+    'pretendard-light': require('./assets/fonts/Pretendard-Light.ttf'),
+    'pretendard-regular': require('./assets/fonts/Pretendard-Regular.ttf'),
+    'pretendard-medium': require('./assets/fonts/Pretendard-Medium.ttf'),
+    'pretendard-semiBold': require('./assets/fonts/Pretendard-SemiBold.ttf'),
+    'pretendard-bold': require('./assets/fonts/Pretendard-Bold.ttf'),
+    'pretendard-extraBold': require('./assets/fonts/Pretendard-ExtraBold.ttf'),
+    'pretendard-black': require('./assets/fonts/Pretendard-Black.ttf')
+  })
+
   return (
     <SafeAreaProvider>
       {/* <Provider store={store}> */}
         <NavigationContainer>
           <StatusBar style="auto" />
-          <AuthRouters />
+          {
+            fontsLoaded && 
+            <SafeAreaView style={styles.cotainer}>
+              <AuthRouters />
+            </SafeAreaView>
+          }
         </NavigationContainer>
       {/* </Provider> */}
     </SafeAreaProvider>
@@ -20,4 +39,8 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  cotainer: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight
+  },
 });
--- front/src/routers/AuthRouters.js
+++ front/src/routers/AuthRouters.js
@@ -1,11 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Routers from "./Routers";
+import OnboardingScreen from "../screens/loading/OnboardingScreen";
+import SignupScreen from "../screens/user/SignupScreen";
 
 const AuthRouters = () => {
   const AuthStack = createStackNavigator();
 
   return (
-    <AuthStack.Navigator>
+    <AuthStack.Navigator screenOptions={{ headerShown: false }} >
+      <AuthStack.Screen name="Onboarding" component={OnboardingScreen} />
+      <AuthStack.Screen name="Signup" component={SignupScreen} />
       <AuthStack.Screen name="Routers" component={Routers} />
     </AuthStack.Navigator>
   )
