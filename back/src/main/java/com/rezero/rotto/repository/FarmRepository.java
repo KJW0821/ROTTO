e68ea81cbadccbc5afec491634be9ff23b607ed8
강주원 <wizuded@naver.com>
2024-05-02 11:23:19 +0900

    feat: 농장 상세 조회 기능 구현

--- back/src/main/java/com/rezero/rotto/repository/FarmRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/FarmRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FarmRepository extends JpaRepository<Farm, Integer> {
+
+    Farm findByFarmCode(int farmCode);
+
 }
