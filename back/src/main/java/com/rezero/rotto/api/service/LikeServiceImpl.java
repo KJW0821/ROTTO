0e584a8e28dc1c4eb9abd21af3819c6a82922c19
KJWKJW <force1394@gmail.com>
2024-05-02 14:25:59 +0900

    feat: Like 서비스 생성

---
+++ back/src/main/java/com/rezero/rotto/api/service/LikeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.rezero.rotto.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LikeServiceImpl implements LikeService {
+}
