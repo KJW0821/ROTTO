1f558f3a780dc15a9b94163f3d2da487c05abc6c
KJWKJW <force1394@gmail.com>
2024-05-02 10:29:01 +0900

    feat: Farm 서비스 작성

---
+++ back/src/main/java/com/rezero/rotto/api/service/FarmServiceImpl.java
@@ -0,0 +1,11 @@
+package com.rezero.rotto.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FarmServiceImpl implements FarmService {
+}
