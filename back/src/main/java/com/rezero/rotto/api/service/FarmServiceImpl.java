28d22bc38e5571843d1b2dfb6c1e330ac6d228dd
강주원 <wizuded@naver.com>
2024-05-02 15:09:47 +0900

    feat: FarmTop10 농장 목록 조회 기능 구현

--- back/src/main/java/com/rezero/rotto/api/service/FarmServiceImpl.java
+++ back/src/main/java/com/rezero/rotto/api/service/FarmServiceImpl.java
@@ -3,9 +3,12 @@ package com.rezero.rotto.api.service;
 import com.rezero.rotto.dto.dto.FarmListDto;
 import com.rezero.rotto.dto.response.FarmDetailResponse;
 import com.rezero.rotto.dto.response.FarmListResponse;
+import com.rezero.rotto.dto.response.FarmTop10ListResponse;
 import com.rezero.rotto.entity.Farm;
+import com.rezero.rotto.entity.FarmTop10;
 import com.rezero.rotto.entity.User;
 import com.rezero.rotto.repository.FarmRepository;
+import com.rezero.rotto.repository.FarmTop10Repository;
 import com.rezero.rotto.repository.UserRepository;
 import com.rezero.rotto.utils.Pagination;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +29,7 @@ public class FarmServiceImpl implements FarmService {
     private final FarmRepository farmRepository;
     private final UserRepository userRepository;
     private final Pagination pagination;
+    private final FarmTop10Repository farmTop10Repository;
 
 
     // 농장 목록 조회
@@ -103,6 +107,39 @@ public class FarmServiceImpl implements FarmService {
                 .awardHistory(farm.getAwardHistory())
                 .beanName(farm.getFarmBeanName())
                 .beanGrade(farm.getFarmBeanGrade())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
+    // 수익률 Top 10 농장 목록 조회
+    public ResponseEntity<?> getRateTop10FarmList(int userCode) {
+        // 해당 유저가 존재하는지 검사
+        User user = userRepository.findByUserCode(userCode);
+        if (user == null || user.getIsDelete()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
+        }
+        // FarmTop10 리스트 가져오기
+        List<FarmTop10> farmTop10List = farmTop10Repository.findAll();
+        // farm 빈 리스트로 생성
+        List<FarmListDto> farms = new ArrayList<>();
+
+        // farmTop10List 순회하며 farmCode 이용하여 정보 불러오고 Dto 에 넣기
+        for (FarmTop10 farmTop10 : farmTop10List) {
+            Farm farm = farmRepository.findByFarmCode(farmTop10.getFarmCode());
+            FarmListDto farmListDto = FarmListDto.builder()
+                    .farmCode(farm.getFarmCode())
+                    .farmName(farm.getFarmName())
+                    .farmLogoPath(farm.getFarmLogoPath())
+                    .beanName(farm.getFarmBeanName())
+                    .build();
+            // farms 에 하나씩 담기
+            farms.add(farmListDto);
+        }
+        // 리스폰스 생성
+        FarmTop10ListResponse response = FarmTop10ListResponse.builder()
+                .farms(farms)
                 .build();
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
