f914ba8be090db2f6a1b0450066ebd79b7742c04
강주원 <wizuded@naver.com>
2024-05-02 11:13:58 +0900

    feat: 모든 리포지토리에 @Repository 어노테이션 추가

--- back/src/main/java/com/rezero/rotto/repository/BlackListRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/BlackListRepository.java
@@ -2,6 +2,8 @@ package com.rezero.rotto.repository;
 
 import com.rezero.rotto.entity.BlackList;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlackListRepository extends CrudRepository<BlackList, String> {
 }
