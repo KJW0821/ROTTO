0bad2e716b6e1a47407287e1d9cc2371b1439586
강주원 <wizuded@naver.com>
2024-05-02 11:04:28 +0900

    feat: 농장 목록 조회 기능 구현(임시로 원두 종류 조회와 필터링, 정렬, 키워드 검색 미포함)

--- back/src/main/java/com/rezero/rotto/api/service/FarmService.java
+++ back/src/main/java/com/rezero/rotto/api/service/FarmService.java
@@ -1,4 +1,9 @@
 package com.rezero.rotto.api.service;
 
+import org.springframework.http.ResponseEntity;
+
 public interface FarmService {
+
+    ResponseEntity<?> getFarmList(int userCode, String sort, String keyword, Integer page);
+
 }
