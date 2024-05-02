193935fc8c342c2e9ee8a323e6aed651fd7f0f83
KJWKJW <force1394@gmail.com>
2024-05-02 16:03:25 +0900

    feat: 알림 상세 조회 기능 구현을 위한 dto 생성

---
+++ back/src/main/java/com/rezero/rotto/dto/response/AlertDetailResponse.java
@@ -0,0 +1,20 @@
+package com.rezero.rotto.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class AlertDetailResponse {
+
+    private String title;
+    private String content;
+    private LocalDateTime createTime;
+
+}
