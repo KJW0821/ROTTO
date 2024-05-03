a43ffa9e75c647a27a5e7ffa5ba21c61fd417e54
박다민 <ferrorist@naver.com>
2024-05-03 16:31:03 +0900

    fix: 토큰 발급 #326

--- smartContract/contracts/TokenManager.sol
+++ smartContract/contracts/TokenManager.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import "./MyStructs.sol";
 import "./interfaces/ITokenCreation.sol";
+import "./interfaces/ITokenDistribute.sol";
 import "./TokenCreation.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -18,6 +19,7 @@ contract TokenManager is Ownable {
         return_rate: 5
     });
     address private tokenCreationAddress;
+    address private tokenDistributeAddress;
 
     event testCreateToken(string message);
     event CheckMsgSender(address addr);
@@ -27,11 +29,19 @@ contract TokenManager is Ownable {
         tokenCreationAddress = _addr;
     }
 
+    function setTokenDistributeAddress(address _addr) public {
+        require(_addr != address(0), unicode"올바르지 않은 주소입니다.");
+        tokenDistributeAddress = _addr;
+    }
 
     // 토큰 생성
     function createToken(uint amount) external onlyOwner {
         emit CheckMsgSender(msg.sender);
         emit testCreateToken("TokenManager.createToken");
         ITokenCreation(tokenCreationAddress).createToken(testSubscription, amount);
+    }
+
+    function distributeToken(uint code, address _wallet, uint amount) external onlyOwner {
+        ITokenDistribute(tokenDistributeAddress).distributeToken(code, _wallet, amount);
     }
 }
