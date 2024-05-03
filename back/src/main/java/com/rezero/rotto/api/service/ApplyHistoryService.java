ec0d6f399a002e1b1d64f47ac375bc49d3445977
옥세훈 <sehun9803@naver.com>
2024-05-03 18:01:09 +0900

    feat: Subscription 연관관계 삭제 수정 및 신청내역 생성

--- back/src/main/java/com/rezero/rotto/api/service/ApplyHistoryService.java
+++ back/src/main/java/com/rezero/rotto/api/service/ApplyHistoryService.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 public interface ApplyHistoryService {
 
     // 청약신청
-    ResponseEntity<?> postApply(int useCode, int subscriptCode);
+    ResponseEntity<?> postApply(int userCode, int subscriptCode);
 
     // 청약 신청 취소
     ResponseEntity<?> deleteApply(int userCode, int applyHistoryCode);
@@ -13,7 +13,7 @@ public interface ApplyHistoryService {
     // 유저의 청약신청 리스트
     ResponseEntity<?> getApplyHistoryListByUser(int userCode, Integer page);
 
-    // 유저의 청얀식청 리스트 상세
+    // 유저의 청약신청 리스트 상세
     ResponseEntity<?> getApplyDetailByUser(int userCode, int subscriptionCode);
 
 }
