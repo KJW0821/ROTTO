e8d3355df250d30935408cb971bce433213e61c8
손유정 <sonyujeong18@gmail.com>
2024-05-05 23:14:01 +0900

    style: 문의 목록 카드 컴포넌트 구현

---
+++ front/src/components/setting/InquiryCard.js
@@ -0,0 +1,42 @@
+import { Pressable, View, Text, StyleSheet } from 'react-native';
+import Colors from '../../constants/Colors';
+
+const InquiryCard = ({navigation, data}) => {
+  return (
+    <Pressable style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.mdFont}>Q.</Text>
+        <Text style={[styles.mdFont, { flex: 1 }]}>{data.title}</Text>
+      </View>
+      <Text style={styles.date}>2024-04-20 19:00</Text>
+    </Pressable>
+  )
+}
+
+export default InquiryCard;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    borderColor: Colors.fontGray,
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 10,
+    gap: 8
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 2
+  },
+  mdFont: {
+    fontSize: 12,
+    fontFamily: 'pretendard-medium'
+  },
+  date: {
+    fontSize: 10,
+    fontFamily: 'pretendard-regular',
+    color: Colors.fontGray,
+    flex: 1,
+    textAlign: 'right'
+  }
+});
