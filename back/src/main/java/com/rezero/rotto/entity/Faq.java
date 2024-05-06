1902c873087a8d46c2f94972deb55bae99aa646a
옥세훈 <sehun9803@gmail.com>
2024-05-06 22:50:48 +0900

    feat: faq. Terms 엔티티 생성

---
+++ back/src/main/java/com/rezero/rotto/entity/Faq.java
@@ -0,0 +1,21 @@
+package com.rezero.rotto.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "faq_tb")
+public class Faq {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faq_code")
+    int faqCode;
+
+
+}
