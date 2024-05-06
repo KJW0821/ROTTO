5883ce6c3fc2e2c3a697ac15a5cd98987a27efac
KJWKJW <force1394@gmail.com>
2024-05-06 22:27:03 +0900

    fix: yml 암호화 과정중 오류 발생 해결

--- back/src/main/java/com/rezero/rotto/config/JasyptConfig.java
+++ back/src/main/java/com/rezero/rotto/config/JasyptConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.key}")
+    @Value("${JASYPT_KEY}")
     private String key;
 
     @Bean("jasyptStringEncryptor")
