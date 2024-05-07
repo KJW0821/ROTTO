e5d55400263a86febb413d58f02e02dab79e8bcb
옥세훈 <sehun9803@naver.com>
2024-05-07 16:48:42 +0900

    fix: @PathVariable 추가(ReqBoard)

--- back/src/main/java/com/rezero/rotto/api/service/ApplyHistoryServiceImpl.java
+++ back/src/main/java/com/rezero/rotto/api/service/ApplyHistoryServiceImpl.java
@@ -7,12 +7,13 @@ import com.rezero.rotto.entity.User;
 import com.rezero.rotto.repository.ApplyHistoryRepository;
 import com.rezero.rotto.repository.SubscriptionRepository;
 import com.rezero.rotto.repository.UserRepository;
-import com.rezero.rotto.utils.Pagination;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +24,11 @@ public class ApplyHistoryServiceImpl implements ApplyHistoryService{
     private final UserRepository userRepository;
     private final SubscriptionRepository subscriptionRepository;
 
+
     @Override
     public ResponseEntity<?> postApply(int userCode, int subscriptCode) {
         User user = userRepository.findByUserCode(userCode);
+        LocalDateTime now = LocalDateTime.now();
         if (user == null || user.getIsDelete()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
         }
@@ -34,7 +37,7 @@ public class ApplyHistoryServiceImpl implements ApplyHistoryService{
         ApplyHistory applyHistory = new ApplyHistory();
         Subscription subscription = subscriptionRepository.findBySubscriptionCode(subscriptCode);
 
-        applyHistory.setApplyTime();
+
 
 
         return null;
