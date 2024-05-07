fa1af0ad9bf902402d70b27666cfbbaa87279988
KJWKJW <force1394@gmail.com>
2024-05-07 17:24:21 +0900

    feat: 필터링 및 정렬을 위해 생성한 JpaSpecification 리포지토리에 주입

--- back/src/main/java/com/rezero/rotto/repository/FarmRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/FarmRepository.java
@@ -2,10 +2,11 @@ package com.rezero.rotto.repository;
 
 import com.rezero.rotto.entity.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FarmRepository extends JpaRepository<Farm, Integer> {
+public interface FarmRepository extends JpaRepository<Farm, Integer>, JpaSpecificationExecutor<Farm> {
 
     Farm findByFarmCode(int farmCode);
 
