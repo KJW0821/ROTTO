a46eebec062b7ba9cb463552404c890e476f1b07
배준형 <wnsgud0895@gmail.com>
2024-05-07 17:56:20 +0900

    wip: discovery 페이지 구현중

--- front/src/routers/Routers.js
+++ front/src/routers/Routers.js
@@ -1,24 +1,30 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeRouters from "./HomeRouters";
 import SettingRouters from "./SettingRouters";
+import DiscoveryRouters from "./DiscoveryRouters";
+import { Ionicons } from "@expo/vector-icons";
 
 const Routers = () => {
   const Tab = createMaterialBottomTabNavigator();
 
   return (
     <Tab.Navigator
+      barStyle={{ backgroundColor: 'white' }}
       initialRouteName="HomePage"
     >
-      <Tab.Screen 
-        name="HomePage"
-        component={HomeRouters} 
-      />
       <Tab.Screen
-        name="SettingPage"
-        component={SettingRouters}
+        name="홈"
+        component={HomeRouters}
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="home-outline" size={24} color="black" />
+          ),
+        }}
       />
+      <Tab.Screen name="발견" component={DiscoveryRouters} />
+      <Tab.Screen name="설정" component={SettingRouters} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 export default Routers;
---
+++ front/src/screens/discovery/DiscoveryScreen.js
@@ -0,0 +1,5 @@
+const DiscoveryScreen = () => {
+
+}
+
+export default DiscoveryScreen
