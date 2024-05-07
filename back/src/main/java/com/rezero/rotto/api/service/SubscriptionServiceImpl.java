cd74c422675ce3f5a430b2b558e4e71638d9169c
옥세훈 <sehun9803@naver.com>
2024-05-07 11:33:57 +0900

    fix: 청약서비스Impl 연관관계 제거

--- back/src/main/java/com/rezero/rotto/api/service/SubscriptionServiceImpl.java
+++ back/src/main/java/com/rezero/rotto/api/service/SubscriptionServiceImpl.java
@@ -3,8 +3,10 @@ package com.rezero.rotto.api.service;
 import com.rezero.rotto.dto.dto.SubscriptionListDto;
 import com.rezero.rotto.dto.response.SubscriptionDetailResponse;
 import com.rezero.rotto.dto.response.SubscriptionListResponse;
+import com.rezero.rotto.entity.Farm;
 import com.rezero.rotto.entity.Subscription;
 import com.rezero.rotto.entity.User;
+import com.rezero.rotto.repository.FarmRepository;
 import com.rezero.rotto.repository.SubscriptionRepository;
 import com.rezero.rotto.repository.UserRepository;
 import com.rezero.rotto.utils.Pagination;
@@ -25,6 +27,7 @@ public class SubscriptionServiceImpl implements SubscriptionService{
     private final SubscriptionRepository subscriptionRepository;
     private final UserRepository userRepository;
     private final Pagination pagination;
+    private final FarmRepository farmRepository;
 
     public ResponseEntity<?> getSubscriptionList(int userCode, Integer page){
         User user = userRepository.findByUserCode(userCode);
@@ -49,14 +52,14 @@ public class SubscriptionServiceImpl implements SubscriptionService{
 
         Collections.reverse(subscriptions);
         List<Subscription> pageSubscriptions = subscriptions.subList(startIdx, endIdx);
-        
+
 
         for (Subscription subscription : pageSubscriptions) {
-
+            Farm farm = farmRepository.findByFarmCode(subscription.getFarmCode());
             SubscriptionListDto subscriptionListDto = SubscriptionListDto.builder()
                     .subscriptionCode(subscription.getSubscriptionCode())
-//                    .farmCode(subscription.getFarm().getFarmCode())
-//                    .farmName(subscription.getFarm().getFarmName())
+                    .farmCode(farm.getFarmCode())
+                    .farmName(farm.getFarmName())
                     .confirmPrice(subscription.getConfirmPrice())
                     .applyCount(subscription.getApplyCount())
                     .endTime(subscription.getEndedTime())
@@ -81,11 +84,13 @@ public class SubscriptionServiceImpl implements SubscriptionService{
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
         }
 
+
         Subscription subscriptionDetail = subscriptionRepository.findBySubscriptionCode(subscriptionCode);
+        Farm farm = farmRepository.findByFarmCode(subscriptionDetail.getFarmCode());
         SubscriptionDetailResponse subscriptionDetailResponse = SubscriptionDetailResponse.builder()
                 .subscriptionCode(subscriptionDetail.getSubscriptionCode())
-//                .farmCode(subscriptionDetail.getFarm().getFarmCode())
-//                .farmName(subscriptionDetail.getFarm().getFarmName())
+                .farmCode(farm.getFarmCode())
+                .farmName(farm.getFarmName())
                 .confirmPrice(subscriptionDetail.getConfirmPrice())
                 .startedTime(subscriptionDetail.getStartedTime())
                 .endTime(subscriptionDetail.getEndedTime())
