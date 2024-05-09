c2201bebc2c83047a0e79f0642fba39a12b5fc3d
KJWKJW <force1394@gmail.com>
2024-05-09 11:24:17 +0900

    feat: 소식 상세 조회를 위한 dto 생성

---
+++ back/src/main/java/com/rezero/rotto/dto/response/NewsDetailResponse.java
@@ -0,0 +1,22 @@
+package com.rezero.rotto.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class NewsDetailResponse {
+
+    private int newsCode;
+    private String title;
+    private String content;
+    private LocalDateTime postTime;
+    private String imgPath;
+
+}
