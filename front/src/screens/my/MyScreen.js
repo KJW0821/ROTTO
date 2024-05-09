de085417fee662fd660b05e0e04b63b29b27160c
손유정 <sonyujeong18@gmail.com>
2024-05-10 01:29:50 +0900

    feat: 전자 지갑 연동 기능 임시 구현

--- front/src/screens/my/MyScreen.js
+++ front/src/screens/my/MyScreen.js
@@ -1,9 +1,40 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import '@walletconnect/react-native-compat'
+import { WalletConnectModal, useWalletConnectModal } from '@walletconnect/modal-react-native'
+
+const projectId = '41c800331b3143bdaddeef0fdefb7852';
+
+const providerMetadata = {
+  name: 'rotto',
+  description: '커피 STO 투자 증권 앱',
+  url: 'exp://192.168.10.4:8081',
+  icons: ['../../../assets/images/skyIcon.png'],
+  redirect: {
+    native: 'exp://192.168.10.4:8081',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+  }
+}
 
 const MyScreen = () => {
+  const { open, isConnected, provider, address } = useWalletConnectModal()
+
+  const pressHandler = () => {
+    if (isConnected) {
+      provider.disconnect();
+    } else {
+      open()
+    }
+  }
+
   return (
     <View>
       <Text>이곳은 마이페이지입니다.</Text>
+      <Button title="지갑 연동" onPress={pressHandler} />
+      {
+        isConnected &&
+        <Text>{address[0]}</Text>
+      }
+      <WalletConnectModal projectId={projectId} providerMetadata={providerMetadata} />
     </View>
   );
 };
