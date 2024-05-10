080d318f1ea939c8947ae70148f4390827f20a3d
배준형 <wnsgud0895@gmail.com>
2024-05-10 17:51:30 +0900

    wip: 상위 10개 농장 조회 작업중

--- front/src/utils/farmAPi.js
+++ front/src/utils/farmAPi.js
@@ -7,29 +7,23 @@ export const getFarmList = async (sort, keyword, ) => {
                
       },
     });
-    console.log(res.data);
     return res.data;
   } catch (err) {
-    console.error(err);
   }
 };
 
 export const getFarmDetail = async (code) => {
   try {
     const res = await API.get(`/farm/${code}`);
-    console.log(res.data);
     return res.data;
   } catch (err) {
-    console.error(err);
   }
 };
 
 export const getFarmTOP10 = async () => {
   try {
     const res = await API.get("/farm/top-ten");
-    console.log(res.data);
     return res.data;
   } catch (err) {
-    console.error(err);
   }
 };
