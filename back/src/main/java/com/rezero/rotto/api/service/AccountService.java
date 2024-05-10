c5bc4f2b0d13934b96355f83879c756734da4a37
옥세훈 <sehun9803@naver.com>
2024-05-10 15:05:50 +0900

    feat: 진짜계좌연결 기능 완료

--- back/src/main/java/com/rezero/rotto/api/service/AccountService.java
+++ back/src/main/java/com/rezero/rotto/api/service/AccountService.java
@@ -1,16 +1,16 @@
 package com.rezero.rotto.api.service;
 
-import com.rezero.rotto.dto.request.AccountCreateRequest;
+import com.rezero.rotto.dto.request.AccountConnectionRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface AccountService {
 
-    // 공모계좌
+    // 공모계좌 조회
     ResponseEntity<?> getAccountZero(int userCode);
 
-    // 연결계좌
+    // 연결계좌 조회
     ResponseEntity<?> getAccountOne(int userCode);
 
-//    // 계좌생성
-//    ResponseEntity<?> postAccountCreate(int userCode, AccountCreateRequest accountCreateRequest);
+    // 진짜계좌연결
+    ResponseEntity<?> postAccountConnection(int userCode, AccountConnectionRequest accountConnectionRequest);
 }
--- back/src/main/java/com/rezero/rotto/dto/response/AccountListOneResponse.java
+++
@@ -1,9 +0,0 @@
-package com.rezero.rotto.dto.response;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Builder
-@Getter
-public class AccountListOneResponse {
-}
