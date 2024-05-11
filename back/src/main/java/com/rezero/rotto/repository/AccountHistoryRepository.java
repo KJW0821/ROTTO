194ec91c0c50293c4ba14d6aae029819e02ff74b
옥세훈 <sehun9803@naver.com>
2024-05-11 11:14:22 +0900

    feat: 입출금내역조회 기능 완료

--- back/src/main/java/com/rezero/rotto/repository/AccountHistoryRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/AccountHistoryRepository.java
@@ -3,5 +3,8 @@ package com.rezero.rotto.repository;
 import com.rezero.rotto.entity.AccountHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountHistoryRepository extends JpaRepository<AccountHistory, Integer> {
+    List<AccountHistory> findByAccountCode(int accountCode);
 }
