49a7768405494b2cdf7cc26fcf72fa29cd6da0a2
손유정 <sonyujeong18@gmail.com>
2024-05-11 23:12:29 +0900

    feat: 공모 계좌 조회 api 호출 함수 구현

---
+++ front/src/utils/accountApi.js
@@ -0,0 +1,13 @@
+import API from "./Api";
+
+const URL = '/account';
+
+export const getAccountInfo = async () => {
+  try {
+    const res = await API.get(URL);
+    return res.data;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
