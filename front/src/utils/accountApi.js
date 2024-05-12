d2a8149a0251533a2161d2dd594ca1f52cbab321
손유정 <sonyujeong18@gmail.com>
2024-05-13 00:14:11 +0900

    feat: 연결 계좌 정보 조회 api 호출 함수 구현

--- front/src/utils/accountApi.js
+++ front/src/utils/accountApi.js
@@ -11,3 +11,13 @@ export const getAccountInfo = async () => {
     return err;
   }
 };
+
+export const getRealAccountInfo = async () => {
+  try {
+    const res = await API.get(URL + '/real');
+    return res.data;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
