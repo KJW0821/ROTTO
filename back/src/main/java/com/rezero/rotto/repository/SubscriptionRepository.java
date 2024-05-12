c67f5649eddfbbb9f31fcead948294fb229eae56
옥세훈 <sehun9803@naver.com>
2024-05-12 22:21:31 +0900

    fix: 신청내역 갯수 추가, 진짜계좌연결 오류 수정

--- back/src/main/java/com/rezero/rotto/repository/SubscriptionRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/SubscriptionRepository.java
@@ -2,10 +2,11 @@ package com.rezero.rotto.repository;
 
 import com.rezero.rotto.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer>, JpaSpecificationExecutor<Subscription> {
 
     // 청약목록상세조회
     Subscription findBySubscriptionCode(int subscriptionCode);
