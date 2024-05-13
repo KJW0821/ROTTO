280831b244dc622b1cec3555384c3f3bd0a5f70c
박다민 <ferrorist@naver.com>
2024-05-13 15:17:37 +0900

    fix: 블록체인 에러 분기 처리 (require, revert)

--- back/src/main/java/com/rezero/rotto/api/controller/BlockChainController.java
+++ back/src/main/java/com/rezero/rotto/api/controller/BlockChainController.java
@@ -37,81 +37,61 @@ public class BlockChainController {
 	@Operation(summary = "ROTTO 생성", description = "입력받은 청약 code에 대한 ROTTO를 입력받은 수 만큼 생성한다.")
 	@PostMapping("/create")
 	public ResponseEntity<?> createToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
-		@RequestBody CreateTokenRequest request) {
-		try {
+		@RequestBody CreateTokenRequest request) throws Exception {
 			int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
 			User user = userRepository.findByUserCode(userCode);
 			if(user.getAdmin())
 				return blockChainService.createToken(request);
 
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("알 수 없는 에러가 발생하였습니다.");
-		}
 	}
 
 	@Operation(summary = "ROTTO 지급", description = "")
 	@PostMapping("/getToken")
-	public ResponseEntity<?> getTokens(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody
-		PayTokensRequest request) {
-		try{
-			int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
-			User user = userRepository.findByUserCode(userCode);
-			if(user.getAdmin())
-				return blockChainService.distributeToken(request);
+	public ResponseEntity<?> getTokens(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
+		@RequestBody PayTokensRequest request) {
+		int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+		User user = userRepository.findByUserCode(userCode);
+		if(user.getAdmin())
+			return blockChainService.distributeToken(request);
 
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
-		} catch (Exception e){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("알 수 없는 에러가 발생하였습니다.");
-		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
 	}
 
 	@Operation(summary = "ROTTO 환급")
 	@PostMapping("/burnToken")
 	public ResponseEntity<?> RefundsToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
 		@RequestBody RefundsTokenRequest request){
-		try{
-			int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
-			User user = userRepository.findByUserCode(userCode);
-			if(user.getAdmin())
-				return blockChainService.RefundsToken(request);
+		int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+		User user = userRepository.findByUserCode(userCode);
+		if(user.getAdmin())
+			return blockChainService.RefundsToken(request);
 
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
-		} catch (Exception e){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("알 수 없는 에러가 발생하였습니다.");
-		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
 	}
 
 	@Operation(summary = "whitelist 추가", description = "입력받은 지갑 주소를 whitelist에 추가한다.")
 	@PostMapping("/insert")
 	public ResponseEntity<?> InsertWhiteList(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody WhiteListRequest wallet){
-		try{
-			int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
-			User user = userRepository.findByUserCode(userCode);
-			if(user.getAdmin()) {
-				String address = wallet.getWallet();
-				return blockChainService.InsertWhiteList(address);
-			}
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
-		} catch (Exception e){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("알 수 없는 에러가 발생하였습니다.");
+		int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+		User user = userRepository.findByUserCode(userCode);
+		if(user.getAdmin()) {
+			String address = wallet.getWallet();
+			return blockChainService.InsertWhiteList(address);
 		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
 	}
 
 	@Operation(summary = "whitelist 제거", description = "입력받은 지갑 주소를 whitelist에 제거한다.")
 	@PutMapping("/remove")
 	public ResponseEntity<?> RemoveWhiteList(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody WhiteListRequest wallet){
-		try{
-			int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
-			User user = userRepository.findByUserCode(userCode);
-			if(user.getAdmin()) {
-				String address = wallet.getWallet();
-				return blockChainService.RemoveWhiteList(address);
-			}
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
-		} catch (Exception e){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("알 수 없는 에러가 발생하였습니다.");
+		int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+		User user = userRepository.findByUserCode(userCode);
+		if(user.getAdmin()) {
+			String address = wallet.getWallet();
+			return blockChainService.RemoveWhiteList(address);
 		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("요청 권한이 없습니다.");
 	}
 
 }
