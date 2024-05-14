761d86ef6ea710330d16360aa1d0b47bb802abb0
손유정 <sonyujeong18@gmail.com>
2024-05-15 04:02:14 +0900

    style: 입출금 화면 style 구현

--- front/src/components/my/AccountHistory.js
+++ front/src/components/my/AccountHistory.js
@@ -72,31 +72,31 @@ const AccountHistory = () => {
           <Ionicons name="reload" size={24} color={Colors.fontGray} />
         </Pressable>
       </View>
-      <ScrollView>
-        {
-          Object.entries(sections).map(([day, content]) => (
-            <View key={day}>
-              <Text>{day}</Text>
-              {
-                content.map((el) => (
-                  <View key={el.accountTime}>
-                    <View>
-                      <FontAwesome name="won" size={24} color="white" />
+      <View style={styles.scrollContainer}>
+        <ScrollView contentContainerStyle={{rowGap: 16}}>
+          {
+            Object.entries(sections).map(([day, content]) => (
+              <View style={styles.dayContainer} key={day}>
+                <Text style={styles.dayText}>{day}</Text>
+                {
+                  content.map((el) => (
+                    <View style={styles.contentContainer} key={el.accountTime}>
+                      <View style={styles.circle}>
+                        <FontAwesome name="won" size={18} color="white" />
+                      </View>
+                      <View style={styles.infoContainer}>
+                        <Text style={styles.transferName}>{el.transferName}</Text>
+                        <Text style={styles.accountTime}>{dayjs(el.accountTime).add(9, 'hour').format("HH:mm")}</Text>
+                      </View>
+                      <Text style={[styles.amount, el.depositOrWithdrawal === 1 && { color: Colors.btnBlue }]}>{el.depositOrWithdrawal === 1 ? '+' : '-'}{el.amount.toLocaleString('ko-KR')} 원</Text>
                     </View>
-                    <View>
-                      <Text>{el.transferName}</Text>
-                      <Text>{dayjs(el.accountTime).add(9, 'hour').format("HH:mm")}</Text>
-                    </View>
-                    <View>
-                      <Text>{el.depositOrWithdrawal === 1 ? '+' : '-'}{el.amount.toLocaleString('ko-KR')} 원</Text>
-                    </View>
-                  </View>
-                ))
-              }
-            </View>
-          ))
-        }
-      </ScrollView>
+                  ))
+                }
+              </View>
+            ))
+          }
+        </ScrollView>
+      </View>
       <FilterModal />
     </View>
   )
@@ -131,5 +131,49 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontFamily: 'pretendard-medium',
     color: Colors.fontGray
+  },
+  dayContainer: {
+    gap: 16
+  },
+  circle: {
+    width: 35,
+    height: 35,
+    borderRadius: 17.5,
+    paddingTop: 2,
+    backgroundColor: Colors.btnBlue,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  dayText: {
+    fontSize: 12,
+    fontFamily: 'pretendard-medium',
+    color: Colors.fontGray
+  },
+  contentContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center'
+  },
+  infoContainer: {
+    gap: 8,
+    flex: 1
+  },
+  transferName: {
+    fontSize: 14,
+    fontFamily: 'pretendard-semiBold'
+  },
+  accountTime: {
+    fontSize: 12,
+    fontFamily: 'pretendard-medium',
+    color: Colors.fontGray
+  },
+  amount: {
+    fontSize: 14,
+    fontFamily: 'pretendard-semiBold',
+    color: '#4F4F4F'
+  },
+  scrollContainer: {
+    width: '95%',
+    marginHorizontal: 'auto'
   }
 });
