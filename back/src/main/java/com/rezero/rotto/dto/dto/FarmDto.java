a42e5c19bdb16141dbac1d776cacaa102cb47442
KJWKJW <force1394@gmail.com>
2024-05-15 19:41:23 +0900

    refactor: 마이페이지 관심농장 조회시 펀딩 여부 반환하고, 농장 목록 조회 코드 더 간결하게 수정

--- back/src/main/java/com/rezero/rotto/dto/dto/FarmDto.java
+++ back/src/main/java/com/rezero/rotto/dto/dto/FarmDto.java
@@ -2,17 +2,13 @@ package com.rezero.rotto.dto.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class FarmListDto extends FarmDto {
+@RequiredArgsConstructor
+public abstract class FarmDto {
 
     private int farmCode;
     private String farmName;
     private String farmLogoPath;
     private String beanName;
-    private Boolean isLiked;
+    private boolean isLiked;
 
 }
