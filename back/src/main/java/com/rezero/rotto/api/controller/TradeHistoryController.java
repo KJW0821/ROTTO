25d562471899443a34f89f453b0ba4f7cb9a2706
옥세훈 <sehun9803@naver.com>
2024-05-15 18:04:22 +0900

    feat: 거래장부 보유 조회 기능 생성, 청약신청시 계좌 입출금 내역생성되도록 수정

--- back/src/main/java/com/rezero/rotto/api/controller/TradeHistoryController.java
+++ back/src/main/java/com/rezero/rotto/api/controller/TradeHistoryController.java
@@ -1,11 +1,40 @@
 package com.rezero.rotto.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rezero.rotto.api.service.TradeHistoryService;
+import com.rezero.rotto.dto.response.SubscriptionListResponse;
+import com.rezero.rotto.dto.response.TradeHistoryListResponse;
+import com.rezero.rotto.utils.JwtTokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequiredArgsConstructor
+@RequestMapping("/trade-history")
+@Tag(name = "ApplyHistory 컨트롤러", description = "정산내역, 보유내역을 위한 API")
 public class TradeHistoryController {
 
+    private final TradeHistoryService tradeHistoryService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Operation(summary = "보유내역 조회", description = "보유내역 목록을 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = TradeHistoryListResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자")
+    })
+
+    @GetMapping
+    public ResponseEntity<?> getTradeHistory(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+        int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+        return tradeHistoryService.getTradeHistory(userCode);
+    }
 
 }
---
+++ back/src/main/java/com/rezero/rotto/api/service/TradeHistoryServiceImpl.java
@@ -0,0 +1,71 @@
+package com.rezero.rotto.api.service;
+
+import com.rezero.rotto.dto.dto.TradeHistoryListDto;
+import com.rezero.rotto.dto.response.TradeHistoryListResponse;
+import com.rezero.rotto.entity.Farm;
+import com.rezero.rotto.entity.Subscription;
+import com.rezero.rotto.entity.TradeHistory;
+import com.rezero.rotto.entity.User;
+import com.rezero.rotto.repository.FarmRepository;
+import com.rezero.rotto.repository.SubscriptionRepository;
+import com.rezero.rotto.repository.TradeHistoryRepository;
+import com.rezero.rotto.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TradeHistoryServiceImpl implements TradeHistoryService{
+
+    private final SubscriptionRepository subscriptionRepository;
+    private final UserRepository userRepository;
+    private final FarmRepository farmRepository;
+    private final TradeHistoryRepository tradeHistoryRepository;
+
+
+    @Override
+    public ResponseEntity<?> getTradeHistory(int userCode) {
+        User user = userRepository.findByUserCode(userCode);
+        if (user == null || user.getIsDelete()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
+        }
+
+        List<TradeHistory> tradeHistories = tradeHistoryRepository.findByUserCode(user.getUserCode());
+        List<TradeHistoryListDto> tradeHistoryListDtos = new ArrayList<>();
+
+        for (TradeHistory tradeHistory: tradeHistories) {
+            Subscription subscription = subscriptionRepository.findBySubscriptionCode(tradeHistory.getSubscriptionCode());
+            Farm farm =farmRepository.findByFarmCode(subscription.getFarmCode());
+
+            TradeHistoryListDto tradeHistoryListDto = TradeHistoryListDto.builder()
+                    .subscriptionCode(subscription.getSubscriptionCode())
+                    .farmName(farm.getFarmName())
+                    .confirmPrice(subscription.getConfirmPrice())
+                    .tradeTime(tradeHistory.getTradeTime())
+                    .tradeNum(tradeHistory.getTradeNum())
+                    .refund(tradeHistory.getRefund())
+                    .totalProceed(subscription.getTotalProceed())
+                    .totalTokenCount(subscription.getTotalTokenCount())
+                    .build();
+
+            tradeHistoryListDtos.add(tradeHistoryListDto);
+        }
+
+        TradeHistoryListResponse response = TradeHistoryListResponse.builder()
+                .tradeHistoryListDtoss(tradeHistoryListDtos)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
+
+}
