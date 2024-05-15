49fb55b7f0c5277024ad5baf08671717bd9b4b90
옥세훈 <sehun9803@naver.com>
2024-05-15 11:15:44 +0900

    feat: 청약생성 기능 완료

--- back/src/main/java/com/rezero/rotto/api/controller/SubscriptionController.java
+++ back/src/main/java/com/rezero/rotto/api/controller/SubscriptionController.java
@@ -1,6 +1,7 @@
 package com.rezero.rotto.api.controller;
 
 import com.rezero.rotto.api.service.SubscriptionService;
+import com.rezero.rotto.dto.request.SubscriptionProduceRequest;
 import com.rezero.rotto.dto.response.SubscriptionDetailResponse;
 import com.rezero.rotto.dto.response.SubscriptionListResponse;
 import com.rezero.rotto.utils.JwtTokenProvider;
@@ -55,5 +56,15 @@ public class SubscriptionController {
         return subscriptionService.getSubscriptionDetail(userCode, subscriptionCode);
     }
 
-
+    @Operation(summary = "청약 생성", description = "청약생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "생성 성공"),
+//                    content = @Content(schema = @Schema(implementation = SubscriptionDetailResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자")
+    })
+    @PostMapping
+    public ResponseEntity<?> postSubscription(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody SubscriptionProduceRequest subscriptionProduceRequest) {
+        int userCode = Integer.parseInt(jwtTokenProvider.getPayload(authorizationHeader.substring(7)));
+        return subscriptionService.postSubscription(userCode, subscriptionProduceRequest);
+    }
 }
