d8db6d16902d897566f8dbe48095fdbc360d216c
옥세훈 <sehun9803@naver.com>
2024-05-15 22:49:12 +0900

    feat: 지출내역, 정산내역 조회 기능 생성 농장엔티티에 총 판매액 추가

--- back/src/main/java/com/rezero/rotto/api/service/TradeHistoryServiceImpl.java
+++ back/src/main/java/com/rezero/rotto/api/service/TradeHistoryServiceImpl.java
@@ -1,18 +1,13 @@
 package com.rezero.rotto.api.service;
 
+import com.rezero.rotto.dto.dto.TradeHistoryExpenseDetailOfSubDto;
 import com.rezero.rotto.dto.dto.TradeHistoryListDto;
+import com.rezero.rotto.dto.response.TradeHistoryExpenseDetailOfSubResponse;
 import com.rezero.rotto.dto.response.TradeHistoryListResponse;
-import com.rezero.rotto.entity.Farm;
-import com.rezero.rotto.entity.Subscription;
-import com.rezero.rotto.entity.TradeHistory;
-import com.rezero.rotto.entity.User;
-import com.rezero.rotto.repository.FarmRepository;
-import com.rezero.rotto.repository.SubscriptionRepository;
-import com.rezero.rotto.repository.TradeHistoryRepository;
-import com.rezero.rotto.repository.UserRepository;
+import com.rezero.rotto.entity.*;
+import com.rezero.rotto.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -29,7 +24,7 @@ public class TradeHistoryServiceImpl implements TradeHistoryService{
     private final UserRepository userRepository;
     private final FarmRepository farmRepository;
     private final TradeHistoryRepository tradeHistoryRepository;
-
+    private final ExpenseDetailRepository expenseDetailRepository;
 
     @Override
     public ResponseEntity<?> getTradeHistory(int userCode) {
@@ -68,6 +63,43 @@ public class TradeHistoryServiceImpl implements TradeHistoryService{
 
     @Override
     public ResponseEntity<?> getExpenseDetailOfSub(int userCode, int subscriptionCode) {
-        return null;
+        User user = userRepository.findByUserCode(userCode);
+        if (user == null || user.getIsDelete()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
+        }
+
+        Subscription subscription = subscriptionRepository.findBySubscriptionCode(subscriptionCode);
+        TradeHistory tradeHistory = tradeHistoryRepository.findByUserCodeAndSubscriptionCode(userCode, subscriptionCode);
+        List<ExpenseDetail> expenseDetails = expenseDetailRepository.findByFarmCode(subscription.getFarmCode());
+        Farm farm =farmRepository.findByFarmCode(subscription.getFarmCode());
+
+        int totalExpenses = expenseDetailRepository.sumExpenseDetailByFarmCode(farm.getFarmCode());
+        List<TradeHistoryExpenseDetailOfSubDto> tradeHistoryExpenseDetailOfSubDtos = new ArrayList<>();
+
+        for (ExpenseDetail expenseDetail : expenseDetails ) {
+            TradeHistoryExpenseDetailOfSubDto tradeHistoryExpenseDetailOfSubDto = TradeHistoryExpenseDetailOfSubDto.builder()
+                    .expenditureContent(expenseDetail.getExpenditureContent())
+                    .expenses(expenseDetail.getExpenses())
+                    .build();
+
+            tradeHistoryExpenseDetailOfSubDtos.add(tradeHistoryExpenseDetailOfSubDto);
+        }
+
+        TradeHistoryExpenseDetailOfSubResponse tradeHistoryExpenseDetailOfSubResponse =TradeHistoryExpenseDetailOfSubResponse.builder()
+                .subscriptionCode(subscription.getSubscriptionCode())
+                .farmName(farm.getFarmName())
+                .totalSoldPrice(farm.getTotalSales())
+                .totalExpense(totalExpenses)
+                .tradeTime(tradeHistory.getTradeTime())
+                .tradeNum(tradeHistory.getTradeNum())
+                .totalProceed(subscription.getTotalProceed())
+                .refund(tradeHistory.getRefund())
+                .totalTokenCount(subscription.getTotalTokenCount())
+                .tradeHistoryExpenseDetailOfSubDtoList(tradeHistoryExpenseDetailOfSubDtos)
+                .build();
+
+
+
+    return ResponseEntity.status(HttpStatus.OK).body(tradeHistoryExpenseDetailOfSubResponse);
     }
 }
---
+++ back/src/main/java/com/rezero/rotto/entity/ExpenseDetail.java
@@ -0,0 +1,27 @@
+package com.rezero.rotto.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "expense_detail_tb")
+public class ExpenseDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "expense_detail_code")
+    private int expenseDetailCode;
+
+    @Column(name = "farm_code")
+    private int farmCode;
+
+    @Column(name = "expenditure _content")
+    private String expenditureContent;
+
+    @Column(name = "expenses")
+    private int expenses;
+}
