86a10655a1121be7e150afd36255c6a7f8643be7
손유정 <sonyujeong18@gmail.com>
2024-05-17 00:46:58 +0900

    style: 펀딩 해지 내역 화면 구현

---
+++ front/src/components/my/CancelList.js
@@ -0,0 +1,76 @@
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import Colors from '../../constants/Colors';
+import dayjs from 'dayjs';
+
+const CancelList = () => {
+
+  return(
+    data &&
+    <FlatList 
+      data={data}
+      renderItem={itemData => {
+        return (
+          <View style={styles.cardContainer}>
+            <View style={styles.innerContainer}>
+              <Text style={styles.farmName}>{itemData.item.farmName}</Text>
+              <Text style={styles.menu}>{dayjs(itemData.item.applyTime).add(9, 'hour').format(YYYY.MM.DD)}</Text>
+            </View>
+            <View style={styles.innerContainer}>
+              <View style={styles.contentContainer}>
+                <Text style={styles.menu}>신청 수량</Text>
+                <Text style={styles.content}>10 / 10 ROT</Text>
+              </View>
+              <Text style={styles.state}>중도 해지</Text>
+            </View>
+          </View>
+        )
+      }}
+      keyExtractor={(item) => {
+        return item.applyHistoryCode
+      }}
+      contentContainerStyle={{ flexGrow: 1 }}
+    />
+  )
+}
+
+export default CancelList;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    justifyContent: 'center',
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 0.5,
+    gap: 3,
+    marginBottom: 10,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  farmName: {
+    fontSize: 12,
+    fontFamily: 'pretendard-medium'
+  },
+  contentContainer: {
+    gap: 3
+  },
+  menu: {
+    fontSize: 8,
+    color: Colors.fontGray,
+    fontFamily: 'pretendard-medium'
+  },
+  content: {
+    fontSize: 10,
+    fontFamily: 'pretendard-medium'
+  },
+  state: {
+    fontSize: 12,
+    fontFamily: 'pretendard-medium',
+    color: 'red'
+  }
+});
