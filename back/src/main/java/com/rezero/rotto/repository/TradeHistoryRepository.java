74930aa44ed9c225addb0b7910611ed4319e4952
옥세훈 <sehun9803@naver.com>
2024-05-16 14:58:03 +0900

    feat: 홈 투자내역 조회 기능 생성

--- back/src/main/java/com/rezero/rotto/repository/TradeHistoryRepository.java
+++ back/src/main/java/com/rezero/rotto/repository/TradeHistoryRepository.java
@@ -3,6 +3,7 @@ package com.rezero.rotto.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.rezero.rotto.entity.TradeHistory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +11,6 @@ public interface TradeHistoryRepository extends JpaRepository<TradeHistory, Inte
 
     List<TradeHistory> findByUserCode(int userCode);
     TradeHistory findByUserCodeAndSubscriptionCode(int userCode, int subscriptionCode);
+
 }
 
