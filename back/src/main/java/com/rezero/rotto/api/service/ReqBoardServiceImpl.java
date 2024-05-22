6f2761880c3ba97a74eb37c6c8d35f85b182d170
KJWKJW <force1394@gmail.com>
2024-05-22 16:51:40 +0900

    refactor: 안쓰는 어노테이션 제거, 유저 검증 로직 수정 및 주석 추가

--- back/src/main/java/com/rezero/rotto/api/service/ReqBoardServiceImpl.java
+++ back/src/main/java/com/rezero/rotto/api/service/ReqBoardServiceImpl.java
@@ -27,9 +27,10 @@ public class ReqBoardServiceImpl implements ReqBoardService {
     private final UserRepository userRepository;
 
     public ResponseEntity<?> getReqBoardList(int userCode) {
+        // 해당 유저가 존재하는지 검사
         User user = userRepository.findByUserCode(userCode);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
+        if (user == null || user.getIsDelete()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 사용자입니다.");
         }
 
         List<ReqBoard> reqBoardList = reqBoardRepository.findByUserCode(user.getUserCode());
