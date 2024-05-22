c3fddad4375795bdef73580445dbd7dbf62f3b01
배준형 <wnsgud0895@gmail.com>
2024-05-22 17:49:06 +0900

    fix: 알림 sse 오류 수정 및 redux 알림 상태 업데이트

--- react-native-cli/rotto/src/components/common/MyHeader.js
+++ react-native-cli/rotto/src/components/common/MyHeader.js
@@ -1,14 +1,15 @@
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
 
-import Colors from "../../constants/Colors";
-import { useDispatch } from "react-redux";
-import { setSearchModal } from "../../stores/fundingSlice";
+import Colors from '../../constants/Colors';
+import {useDispatch, useSelector} from 'react-redux';
+import {setSearchModal} from '../../stores/fundingSlice';
 
-const MyHeader = ({ children, isSearch }) => {
+const MyHeader = ({children, isSearch}) => {
   const Navigation = useNavigation();
   const dispatch = useDispatch();
+  const isAlert = useSelector(state => state.alertInfo.isAlert);
 
   const onPress = () => {
     dispatch(setSearchModal(true));
@@ -19,29 +20,31 @@ const MyHeader = ({ children, isSearch }) => {
       <View style={styles.imageContainer}>
         <Image
           style={styles.appLogo}
-          source={require("../../../assets/images/RottoLogo.png")}
+          source={require('../../../assets/images/RottoLogo.png')}
         />
-        {
-          isSearch ?
+        {isSearch ? (
           <Pressable style={styles.searchIcon} onPress={onPress}>
             <Ionicons name="search" size={24} color="white" />
           </Pressable>
-          :
+        ) : (
           <View style={styles.iconContainer}>
-            <Ionicons
-              name="notifications-outline"
-              size={26}
-              color="white"
-              onPress={() => Navigation.navigate("alertList")}
-            />
+            <View>
+              <Ionicons
+                name="notifications-outline"
+                size={26}
+                color="white"
+                onPress={() => Navigation.navigate('alertList')}
+              />
+              {isAlert && <View style={styles.alertPointer}></View>}
+            </View>
             <Ionicons
               name="person-outline"
               size={26}
               color="white"
-              onPress={() => Navigation.navigate("My")}
+              onPress={() => Navigation.navigate('My')}
             />
           </View>
-        }
+        )}
       </View>
       {children}
     </View>
@@ -59,22 +62,30 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.bgOrg,
   },
   imageContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   iconContainer: {
     width: 60,
     marginRight: 20,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   text: {
-    color: "white",
+    color: 'white',
   },
   searchIcon: {
     marginRight: 20,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  alertPointer: {
+    position: 'absolute',
+    right: 0,
+    backgroundColor: 'red',
+    width: 4,
+    height: 4,
+    borderRadius: 2,
   }
 });
