d991c632c9a4237232c5cbd75ee66794b8c32238
KJWKJW <force1394@gmail.com>
2024-05-24 12:17:19 +0900

    refactor: Firebase 초기화 코드 깔끔하게 리팩토링

--- back/src/main/java/com/rezero/rotto/config/FirebaseConfig.java
+++ back/src/main/java/com/rezero/rotto/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.rezero.rotto.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -14,32 +15,23 @@ import java.io.InputStream;
 
 // Firebase 설정을 관리하는 Config
 @Configuration
+@Slf4j
 public class FirebaseConfig {
 
-    public static final String FIREBASE_ALARM_SEND_API_URI = "https://fcm.googleapis.com/v1/projects/rotto-4aa74/messages:send";
-    public static final String FIREBASE_CONFIG_PATH = "serviceAccountKey.json";
     private final ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
 
-    /**
-     * resources/serviceAccountKey.json
-     * java/com/rezero/rotto/config/FirebaseConfig.java
-     * **/
 
     // Firebase 를 초기화하는 메서드
     @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        InputStream serviceAccount = null;
-        try {
-            // 클래스패스로부터 리소스를 가져옴
-            serviceAccount = resource.getInputStream();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public FirebaseApp initializeFirebase() {
+        try (InputStream serviceAccount = resource.getInputStream()) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
             return FirebaseApp.initializeApp(options);
-
+        } catch (IOException e) {
+            log.error("Failed to initialize FirebaseApp", e);
+            return null;
+        }
     }
 }
