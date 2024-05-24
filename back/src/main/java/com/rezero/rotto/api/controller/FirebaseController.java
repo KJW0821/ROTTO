fa8c5f76c9f1bfe1abfdc4b7d9c82da66b6186f2
KJWKJW <force1394@gmail.com>
2024-05-24 15:24:51 +0900

    feat: 클라이언트가 요청한 푸시 알림 전송하는 기능 구현

--- back/src/main/java/com/rezero/rotto/api/controller/FirebaseController.java
+++ back/src/main/java/com/rezero/rotto/api/controller/FirebaseController.java
@@ -1,17 +1,15 @@
 package com.rezero.rotto.api.controller;
 
 import com.rezero.rotto.api.service.FirebaseService;
-import com.rezero.rotto.dto.request.FirebaseNotificationRequest;
+import com.rezero.rotto.dto.request.SendMessageToAllUsersRequest;
+import com.rezero.rotto.dto.request.SendMessageToRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,15 +20,25 @@ public class FirebaseController {
     private final FirebaseService firebaseService;
 
 
-    @Operation(summary = "요청에 대한 알림 전송", description = "해당 요청이 들어올 때 알림 전송")
+    @Operation(summary = "특정 유저에게 푸시 알림 전송", description = "해당 요청이 들어올 때 알림 전송")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 기기에 대한 요청")
+    })
+    @PostMapping
+    public ResponseEntity<?> sendMessageTo(@RequestBody SendMessageToRequest request) {
+        return firebaseService.sendMessageTo(request);
+    }
+
+
+    @Operation(summary = "모든 유저에게 푸시 알림 전송", description = "해당 요청이 들어올 때 알림 전송")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공"),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자")
     })
-    @GetMapping
-    public ResponseEntity<?> sendNotification(@RequestBody FirebaseNotificationRequest request) {
-        firebaseService.sendMessage(request.getTopic(), request.getTitle(), request.getBody());
-        return ResponseEntity.ok().build();
+    @PostMapping()
+    public ResponseEntity<?> sendMessageToAllUsers(@RequestBody SendMessageToAllUsersRequest request) {
+        return firebaseService.sendMessageToAllUsers(request);
     }
 
 }
